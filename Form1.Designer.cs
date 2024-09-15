0343ac90262d0cf4379315f8e7af75414b769696
“shmenkare” <shmenkare@gmail.com>
2024-09-14 22:55:28 -0400

    Commit inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvLista = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lsvLista
+            // 
+            this.lsvLista.Location = new System.Drawing.Point(12, 42);
+            this.lsvLista.Name = "lsvLista";
+            this.lsvLista.Size = new System.Drawing.Size(420, 363);
+            this.lsvLista.TabIndex = 0;
+            this.lsvLista.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 429);
+            this.Controls.Add(this.lsvLista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvLista;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reader
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataReader rd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Read()
+        {
+            string cadena = "Data Source=.;Initial Catalog=MusicListDB;Integrated Security=True";
+            string sql = "SELECT * FROM Temas";
+            con = new SqlConnection(cadena);
+            con.Open();
+            cmd = new SqlCommand(sql, con);
+            rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                lsvLista.Items.Add(rd[1].ToString());
+            }
+            rd.Close();
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read();
+        }
+    }
+
+}
+
+
+
