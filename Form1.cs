7bb7da95ee6e5f5e1f0d7ff85c920ccffe626fa6
Orion Ra <126419073+shmenkare@users.noreply.github.com>
2024-09-17 00:30:07 -0400

    agregando datacolumns

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,9 @@ namespace Reader
         private SqlDataReader rd;
         private DataSet ds;
         private DataTable dt;
+        private DataColumn id;
+        private DataColumn name;
+        private DataColumn age;
 
         public Form1()
         {
@@ -29,16 +32,27 @@ namespace Reader
 
             ds = new DataSet("DataBase");
             dt = new DataTable("Nombres");
+            
+            id = new DataColumn("Id", typeof(int));
+            id.AutoIncrement = true;
+            id.AutoIncrementSeed = 1;
+            id.AutoIncrementStep = 1;
+           
+            name = new DataColumn("Nombre", typeof(string));
+            age = new DataColumn("Edad",typeof(int));
+            
+            dt.Columns.AddRange(new DataColumn[] { id, name, age });
+            dt.PrimaryKey = new DataColumn[] { id };
+            
             ds.Tables.Add(dt);
-            dt.Columns.Add("Id", typeof(int));
-            dt.Columns.Add("Nombre", typeof(string));
-            dt.Columns.Add("Edad", typeof(int));
-            dt.Rows.Add(1,"Orion",50);
-            dt.Rows.Add(2, "Vader", 55);
-            dt.Rows.Add(3, "Shmenkare", 35);
-            dt.Rows.Add(4, "Fett", 40);
-            dt.Rows.Add(5, "Han Solo", 35);
-            dt.Rows.Add(6, "Maulth", 30);
+            
+            dt.Rows.Add(null,"Orion",50);
+            dt.Rows.Add(null,"Vader", 55);
+            dt.Rows.Add(null,"Shmenkare", 35);
+            dt.Rows.Add(null,"Fett", 40);
+            dt.Rows.Add(null,"Han Solo", 35);
+            dt.Rows.Add(null,"Maulth", 30);
+            dt.Rows.Add(null, "Acvar", 65);
 
             dgvDatos.DataSource = dt;
 
